77f0c5ef22ed42f18d8a13771a3fd572cebe0ab9
ikhsan <ikhsancahyamardika@gmail.com>
2025-06-25 22:00:47 +0700

    fix cd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const {greeting} = require('./user')
 
-app.get('/', (req,res) => res.send('This App is running properly!'))
+app.get('/', (req,res) => res.send('This App Continuous Deployment is running properly!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
 app.get('/youtube', (req,res) => res.send('Hello, youtube Ikhsan!'))
 app.get('/hello/:name', (req,res) => {
